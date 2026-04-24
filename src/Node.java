ebdb16cba7945208c782cda68836e8c62e435437
Julia Cunha Hedin <julia.chedin@gmail.com>
2026-04-24 22:30:27 +0200

    fields and getter methods

--- src/Node.java
+++ src/Node.java
@@ -1,3 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Node{
-    
+    private List<Node> neighbours = new ArrayList<>();
+    private int type; 
+
+
+    /**
+     * displays all connecting nodes
+     * @return connecting nodes
+     */
+    public List<Node> getNeighbours() {
+        return neighbours;
+    }
+    /**
+     * indicates if the node represents a wall, path, start or end point
+     * @return integer of state
+     */
+    public int getType(){
+        return type;
+    }
+
 }
