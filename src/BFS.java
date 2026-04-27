066d5fa78c6d889c69ffe92eeff7485c50db1e1c
Julia Cunha Hedin <julia.chedin@gmail.com>
2026-04-27 20:34:46 +0200

    initialize method and fields

--- src/BFS.java
+++ src/BFS.java
@@ -1,9 +1,23 @@
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.Set;
 
 public class BFS implements PathfindingAlgorithm{
     
+    private Set<Node> visited;
+    private Queue<Node> queue;
+    private Node endNode;
+
+    public BFS(){
+        visited = new HashSet<>();
+        queue = new LinkedList<Node>();
+    }
+
     public void initialize(Node start, Node end){
-        
+        endNode = end;
+        queue.add(start);
     }
 
     public void step(){
