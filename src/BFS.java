3e03ea111481c607dcfe9de324cfdc1d942045ea
Julia Cunha Hedin <julia.chedin@gmail.com>
2026-04-27 18:43:54 +0200

    setting up the interface

---
+++ src/BFS.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+public class BFS implements PathfindingAlgorithm{
+    
+    public void initialize(Node start, Node end){
+        
+    }
+
+    public void step(){
+
+    }
+
+    public boolean isFinished(){
+        return true;
+    }
+
+    public boolean hasPath(){
+        return true;
+    }    
+
+    public List<Node> visitedNodes(){
+        
+    }
+
+    public List<Node> foundPath(){
+
+    }
+}
