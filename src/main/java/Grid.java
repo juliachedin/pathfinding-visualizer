7d51da7f367b2d5bbc7fdb7250767ce0bcc7a1bb
Julia Cunha Hedin <julia.chedin@gmail.com>
2026-04-29 18:04:46 +0200

    possible to reset all nodes in grid and access nodes

--- src/main/java/Grid.java
+++ src/main/java/Grid.java
@@ -7,7 +7,7 @@ public class Grid{
     public Grid(int width, int height){
         xSize = width;
         ySize = height;
-
+        // creates a grid with the specified width and height
         for (int i = 0; i < width; i++){
             for (int j = 0; j < height; j++){
                 //default value 0 for type, indicating "free"
@@ -16,6 +16,34 @@ public class Grid{
             }
         }
     }
+    /**
+     * This method cleans all of the nodes by removing all connections and walls
+     */
+    public void cleanGrid(){
+        for (int i = 0; i < xSize; i++){
+            for (int j = 0; j < ySize; j++){
+                //removes the parent 
+                nodes[i][j].setParent(null);
+                //returns to default type of being "free"
+                nodes[i][j].setType(0);
+                //removes all connections
+                for(Node neighbour: nodes[i][j].getNeighbours()){
+                    nodes[i][j].removeNeighbour(neighbour);
+                }
+            }
+        }
+    }
+    /**
+     * Method for getting a specific node in the grid
+     * @param xIndex x-index
+     * @param yIndex y-index
+     * @return node asked for
+     */
+    public Node getNode(int xIndex, int yIndex){
+        return nodes[xIndex][yIndex];
+    }
+
+
 
 
 }
