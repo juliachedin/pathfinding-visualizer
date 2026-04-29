b0ac7c234e98c073259143003cf7b3df9bef3312
Julia Cunha Hedin <julia.chedin@gmail.com>
2026-04-29 19:46:04 +0200

    creates a grid of rectangles

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,27 @@
+import javafx.scene.paint.Color;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.scene.shape.Rectangle;
+
 
 //Must extend application to use JavaFX functionality
 public class Main extends Application{
 
-    Button button;
+    private int width = 800;
+    private int height = 700;
+    private int squareSize = 20;
+    private int numCellsX = 20;
+    private int numCellsY = 20;
+   
+    private Rectangle [][] gridUI;
+    private Grid grid;
+    private Pane gridPane;
+    
 
     public static void main(String[] args) {
         launch(args); //set up in the application 
@@ -19,16 +31,34 @@ public class Main extends Application{
     @Override 
     public void start(Stage primaryStage) throws Exception{
         //Terminology: the window is the stage and the content inside is the scene
-        primaryStage.setTitle("title of the window");
-        
-        button = new Button("Click me");
+        primaryStage.setTitle("Pathfinding Visualizer");
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
+        gridPane = new Pane();
+        grid = new Grid(numCellsX, numCellsY);
 
-        Scene scene = new Scene(layout, 300, 250);
+        createVisualGrid();
+
+        Scene scene = new Scene(gridPane, width, height);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+    public void createVisualGrid(){
+        gridUI = new Rectangle[numCellsX][numCellsY];
+
+        for (int i = 0; i < numCellsX; i++){
+            for (int j = 0; j < numCellsY; j++){
+                Rectangle rect = new Rectangle(squareSize, squareSize);
+                rect.setX(i * squareSize);
+                rect.setY(j * squareSize);
+                
+                rect.setFill(Color.WHITE);
+                rect.setStroke(Color.BLACK);
+
+                gridUI[i][j] = rect;
+
+                gridPane.getChildren().add(rect);
+            }
+        }
     }
 
 }
